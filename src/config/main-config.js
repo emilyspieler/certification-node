2a0b9679d9454aa20180a44931ff351f2e7fdcf2
emilyspieler <emilyspieler1@gmail.com>
2019-01-23 20:27:54 -0600

    User: complete

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const flash = require("express-flash");
 const async = require('async');
 const expressValidator = require("express-validator");
+const passportConfig = require("./passport-config");
 
 module.exports = {
 
@@ -17,6 +18,19 @@ module.exports = {
          app.use(bodyParser.urlencoded({ extended: true }));
          app.use(express.static(path.join(__dirname, "..", "assets")));
          app.use(expressValidator());
+         app.use(session({
+           secret: process.env.cookieSecret,
+           resave: false,
+           saveUninitialized: false,
+           cookie: { maxAge: 1.21e+9 } //set cookie to expire in 14 days
+         }));
+   app.use(flash());
+   passportConfig.init(app);
+
+   app.use((req,res,next) => {
+     res.locals.currentUser = req.user;
+     next();
+   })
 
   }
 };
