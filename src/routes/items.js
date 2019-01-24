691e1cf433bd700f01778d590216270cb70f0a2f
emilyspieler <emilyspieler1@gmail.com>
2019-01-23 20:47:41 -0600

    user validations

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const validation = require("./validation");
 
 const itemController = require("../controllers/itemController");
 
 router.get("/lists/:listId/items/new", itemController.new);
-router.post("/lists/:listId/items/create", itemController.create);
+router.post("/lists/:listId/items/create", validation.validateItems, itemController.create);
 router.get("/lists/:listId/items/:id", itemController.show);
 router.post("/lists/:listId/items/:id/destroy", itemController.destroy);
 router.get("/lists/:listId/items/:id/edit", itemController.edit);
-router.post("/lists/:listId/items/:id/update", itemController.update);
+router.post("/lists/:listId/items/:id/update", validation.validateItems, itemController.update);
 
 
 module.exports = router;
