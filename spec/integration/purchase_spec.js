8aea5f8a0c678b944a1ee6f39f0f6885763397c9
emilyspieler <emilyspieler1@gmail.com>
2019-01-28 10:57:31 -0600

    additional tdd

--- spec/integration/purchase_spec.js
+++ spec/integration/purchase_spec.js
@@ -69,6 +69,24 @@ describe("routes : purchases", () => {
       });
     });
 
+    describe("POST /lists/:listId/items/:itemId/purchases/create", () => {
+
+      it("should not create a new purchase", () => {
+        const options = {
+          url: `${base}${this.list.id}/items/${this.item.id}/purchases/create`
+        };
+
+        let purCountBeforeCreate;
+
+        this.item.getPurchases()
+        .then((purchases) => {
+          purCountBeforeCreate = purchases.length;
+
+        });
+      });
+
+    });
+
     describe("signed in user favoriting a post", () => {
 
      beforeEach((done) => {  // before each suite in this context
@@ -85,5 +103,64 @@ describe("routes : purchases", () => {
        );
       });
      });
+
+     describe("POST /lists/:listId/items/:itemId/purchases/create", () => {
+
+       it("should create a purchase", () => {
+         const options = {
+           url: `${base}${this.list.id}/items/${this.item.id}/purchases/create`
+         };
+         request.post(options,
+           (err, res, body) => {
+             Purchase.findOne({
+               where: {
+                 userId: this.user.id,
+                 itemId: this.item.id
+               }
+             })
+             .then((purchase) => {               // confirm that a favorite was created
+               expect(purchase).not.toBeNull();
+               expect(purchase.userId).toBe(this.user.id);
+               expect(purchase.itemId).toBe(this.item.id);
+               done();
+             })
+             .catch((err) => {
+               console.log(err);
+               done();
+             });
+           }
+         );
+       });
+     });
+
+     describe("POST /lists/:listId/items/:itemId/purchases/:id/destroy", () => {
+
+       it("should destroy a purchase", () => {
+         const options = {
+           url: `${base}${this.list.id}/items/${this.item.id}/purchases/create`
+         };
+
+         let purCountBeforeDelete;
+
+         request.post(options, (err, res, body) => {
+           this.post.getPurchases()
+           .then((purhcases) => {
+             const purchase = purchases[0];
+             purCountBeforeDelete = purchases.length;
+
+             request.post(`${base}${this.list.id}/items/${this.item.id}/purchases/${purchase.id}/destroy`,
+               (err, res, body) => {
+                 this.item.getPurchases()
+                 .then((purchases) => {
+                   expect(purchases.length).toBe(purCountBeforeDelete - 1);
+                   done();
+                 });
+               }
+             );
+           });
+         });
+       });
+     });
+
 
 });
